ab5456aaddd59fac647fc75858a8c55459b1bd28
Skullking2520 <hyunjunsoo1@gmail.com>
2025-02-27 23:06:49 +0900

    Update practitioner_link.py

--- practitioner_link.py
+++ practitioner_link.py
@@ -83,55 +83,58 @@ def main():
     ph = ProcessHandler(progress_sheet, {"progress": "setting", "UrlNum": 1}, "A1")
     progress = ph.load_progress()
     set_link_sheet(link_sheet)
-    link_sheet.update([["Running Scrapping"]], "C1")
-
-    progress["progress"] = "processing"
-    driver.get(URL)
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    wait_for_page_load(driver)
-    search_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Search']")))
-    search_button.click()
-    page_buttons = find_elements(driver, ".//button[contains(@kx-type, 'underline')]")
-    page_numbers = []
-    links = []
-
-    for page_button in page_buttons:
-        page_numbers.append(page_button.text)
-
-    print("Starting scrapping link")
-
-    while str(progress["UrlNum"]) in page_numbers:
-        page_buttons = find_elements(driver, ".//button[contains(@kx-type, 'underline')]")
-        for page_button in page_buttons:
-            if page_button.text == str(progress["UrlNum"]):
-                page_button.click()
-                break
+    if  progress["progress"] != "finished":
+        link_sheet.update([["Running Scrapping"]], "C1")
+    
+        progress["progress"] = "processing"
+        driver.get(URL)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         wait_for_page_load(driver)
-        practitioners = find_elements(driver, "//lightning-layout-item[contains(@class, 'search-result-style')]")
-
-        print(f"Page: {progress["UrlNum"]}")
-
-        for practitioner in practitioners:
-            head = find_element(practitioner, ".//a[contains(@class, 'search-result-name-text-style')]")
-            if head != "N/A":
-                name = head.text
-                link = head.get_attribute("href")
-            else:
-                name = "Failed to load practitioner"
-                link = "Failed to load practitioner link"
-            links.append([name, link])
-
-        batch_append_multiple_rows(link_sheet, links)
+        search_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Search']")))
+        search_button.click()
+        page_buttons = find_elements(driver, ".//button[contains(@kx-type, 'underline')]")
+        page_numbers = []
         links = []
-        progress["UrlNum"] += 1
-
-    progress["progress"] = "finished"
-    progress["UrlNum"] = 1
-    ph.save_progress(progress)
-
-    link_sheet.update([["Finished Scrapping"]], "C1")
-    print("Scraping link finished")
-    driver.quit()
+    
+        for page_button in page_buttons:
+            page_numbers.append(page_button.text)
+    
+        print("Starting scrapping link")
+    
+        while str(progress["UrlNum"]) in page_numbers:
+            page_buttons = find_elements(driver, ".//button[contains(@kx-type, 'underline')]")
+            for page_button in page_buttons:
+                if page_button.text == str(progress["UrlNum"]):
+                    page_button.click()
+                    break
+            wait_for_page_load(driver)
+            practitioners = find_elements(driver, "//lightning-layout-item[contains(@class, 'search-result-style')]")
+    
+            print(f"Page: {progress["UrlNum"]}")
+    
+            for practitioner in practitioners:
+                head = find_element(practitioner, ".//a[contains(@class, 'search-result-name-text-style')]")
+                if head != "N/A":
+                    name = head.text
+                    link = head.get_attribute("href")
+                else:
+                    name = "Failed to load practitioner"
+                    link = "Failed to load practitioner link"
+                links.append([name, link])
+    
+            batch_append_multiple_rows(link_sheet, links)
+            links = []
+            progress["UrlNum"] += 1
+    
+        progress["progress"] = "finished"
+        progress["UrlNum"] = 1
+        ph.save_progress(progress)
+    
+        link_sheet.update([["Finished Scrapping"]], "C1")
+        print("Scraping link finished")
+        driver.quit()
+    else:
+        print("Finished already")
 
 if __name__ == "__main__":
     main()
